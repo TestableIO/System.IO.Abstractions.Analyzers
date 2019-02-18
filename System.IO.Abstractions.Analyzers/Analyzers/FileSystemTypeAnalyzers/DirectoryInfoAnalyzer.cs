aa90c732c03512f026e4ca188916481dae237e10
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2019-02-19 00:17:32 +0300

    DirectoryInfo Analyzer

---
+++ System.IO.Abstractions.Analyzers/Analyzers/FileSystemTypeAnalyzers/DirectoryInfoAnalyzer.cs
@@ -0,0 +1,56 @@
+using System.Collections.Immutable;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace System.IO.Abstractions.Analyzers.Analyzers.FileSystemTypeAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DirectoryInfoAnalyzer: BaseFileSystemNodeObjectCreationAnalyzer
+	{
+		/// <summary>
+		/// Diagnostic Identifier
+		/// </summary>
+		[UsedImplicitly]
+		public const string DiagnosticId = "IO0007";
+
+		/// <summary>
+		/// Diagnostic Title
+		/// </summary>
+		private const string Title = "Invocation FileStream class shold be replaced with IFileSystem.FileStream";
+
+		/// <summary>
+		/// Diagnostic Message Format
+		/// </summary>
+		public const string MessageFormat = Title;
+
+		/// <summary>
+		/// Diagnostic Description
+		/// </summary>
+		private const string Description = Title;
+
+		/// <summary>
+		/// Правило
+		/// </summary>
+		private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId,
+			Title,
+			MessageFormat,
+			Category,
+			DiagnosticSeverity.Warning,
+			true,
+			Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		protected override void Analyze(SyntaxNodeAnalysisContext context, ObjectCreationExpressionSyntax syntax)
+		{
+			context.ReportDiagnostic(Diagnostic.Create(Rule, syntax.GetLocation()));
+		}
+
+		protected override Type GetFileSystemType()
+		{
+			return typeof(DirectoryInfo);
+		}
+	}
+}

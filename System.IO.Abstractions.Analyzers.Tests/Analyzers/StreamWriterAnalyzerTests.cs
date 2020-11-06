a6a5ca76ad2f9012eb305b4d747448a2877d5c1d
LukasGelke <71026023+LukasGelke@users.noreply.github.com>
2020-11-06 17:43:37 +0100

    add new analyzers for StreamReader / StreamWriter (#83)
    - for #81

---
+++ System.IO.Abstractions.Analyzers.Tests/Analyzers/StreamWriterAnalyzerTests.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.IO.Abstractions.Analyzers.Analyzers.FileSystemTypeAnalyzers;
+using Microsoft.CodeAnalysis;
+using Roslyn.Testing.Analyzer;
+using Roslyn.Testing.Model;
+using Xunit;
+
+namespace System.IO.Abstractions.Analyzers.Tests.Analyzers
+{
+	public class StreamWriterAnalyzerTests : CSharpDiagnosticAnalyzerTest<StreamWriterAnalyzer>
+	{
+		[Theory]
+		[InlineData("UsingStream.txt")]
+		public void Analyzer_is_not_triggered(string filename)
+		{
+			var source = ReadFile(filename);
+			VerifyNoDiagnosticTriggered(source);
+		}
+
+		[Theory]
+		[InlineData("UsingFilename.txt", 15, 19)]
+		[InlineData("UsingFilenameAndOtherParameters.txt", 16, 19)]
+		public void Analyzer_is_triggered(string filename, int diagnosticLine, int diagnosticColumn)
+		{
+			var source = ReadFile(filename);
+
+			var expectedDiagnostic = new DiagnosticResult
+			{
+				Id = StreamWriterAnalyzer.DiagnosticId,
+				Message = StreamWriterAnalyzer.MessageFormat,
+				Severity = DiagnosticSeverity.Warning,
+				Locations = new[] { new DiagnosticResultLocation("Test0.cs", diagnosticLine, diagnosticColumn) }
+			};
+
+			VerifyDiagnostic(source, expectedDiagnostic);
+		}
+
+		[Fact]
+		public void Empty_source_code_does_not_trigger_analyzer()
+		{
+			var source = string.Empty;
+			VerifyNoDiagnosticTriggered(source);
+		}
+
+		protected override IEnumerable<MetadataReference> GetAdditionalReferences() => new[]
+		{
+			MetadataReference.CreateFromFile(typeof(IFileSystem).Assembly.Location)
+		};
+	}
+}
--- System.IO.Abstractions.Analyzers/Analyzers/BaseFileSystemNodeAnalyzer.cs
+++ System.IO.Abstractions.Analyzers/Analyzers/BaseFileSystemNodeAnalyzer.cs
@@ -95,5 +95,13 @@ namespace System.IO.Abstractions.Analyzers.Analyzers
 				&& !namespaceSymbol.IsGlobalNamespace
 				&& namespaceSymbol.ToString().StartsWith(Constants.FileSystemNameSpace, StringComparison.Ordinal);
 		}
+
+		protected static bool IsFirstConstructorParameterOfType<T>(SyntaxNodeAnalysisContext context, ExpressionSyntax syntax)
+		{
+			return (syntax as ObjectCreationExpressionSyntax)?.ArgumentList?.Arguments.FirstOrDefault() is ArgumentSyntax firstArgument
+				&& (context.SemanticModel.GetSymbolInfo(firstArgument.Expression).Symbol as ILocalSymbol)?.Type is ITypeSymbol argumentType
+				&& argumentType.ContainingNamespace.Name == typeof(T).Namespace
+				&& argumentType.Name == typeof(T).Name;
+		}
 	}
 }
